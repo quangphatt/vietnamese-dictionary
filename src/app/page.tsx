4e1c79f8bdf26b205f73a44d69f5f1e328e31417
quangphatt <phatquang2811@gmail.com>
2025-12-28 23:19:18 +0700

    Loading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from "react";
 import Home from "@/views/home";
+import { Loading } from "@/components/Loading";
 
 const HomePage = () => {
-  return <Home />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <Home />
+    </Suspense>
+  );
 };
 
 export default HomePage;
